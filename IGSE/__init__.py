fe0237e8992509a9fa2ca9f20d3b45bf29466685
Joseph <josephomoregie1@outlook.com>
2023-01-05 00:58:47 +0000

    created error handling file and pages

--- IGSE/__init__.py
+++ IGSE/__init__.py
@@ -18,7 +18,10 @@
 
 from flask import Flask
 app = Flask(__name__)
-
 from IGSE.Core.views import core
+from IGSE.error_pages.handlers import error_pages
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
+
+
 
