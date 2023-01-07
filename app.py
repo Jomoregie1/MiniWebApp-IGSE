766cec0c405aac7bb216e22ee7cf2962bd7eeb56
Joseph <josephomoregie1@outlook.com>
2023-01-07 14:36:20 +0000

    added registration form template to register.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from IGSE import app
 from IGSE.models import create_admin_account
 
-create_admin_account(email='gse@shangrilla.gov.un', password='gse@energy')
+# create_admin_account(email='gse@shangrilla.gov.un', password='gse@energy')
 
 if __name__ == '__main__':
     app.run(debug=True)
